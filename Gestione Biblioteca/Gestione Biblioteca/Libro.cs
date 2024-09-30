f9b9b8866cea3855b7f37f76480c6bbd4fd99ba7
QuagliettaS4D <quagliettasamuel@gmail.com>
2024-09-30 13:42:48 +0200

    Aggiunto File

---
+++ Gestione Biblioteca/Gestione Biblioteca/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestione_Biblioteca
+{
+    class Libro
+    {
+        //proprietà
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int Pagine { get; set; }
+        public decimal Prezzo { get; set; }
+        private int CodiceSegreto { get; }
+
+        //costruttore
+        public Libro() 
+        {
+            //questo blocco di codice viene eseguito alla creazione (new) degli oggetti di questa classe
+            CodiceSegreto = 321;
+        }
+
+        public decimal PrezzoInDollari()
+        {
+            decimal prezzoConvertito = Prezzo * 0.89M;
+            return prezzoConvertito;
+        }
+    }
+}
---
+++ Gestione Biblioteca/Gestione Biblioteca/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Gestione_Biblioteca;
+
+Console.WriteLine("Gestione Biblioteca");
+Console.WriteLine("Inserimento libri");
+
+Libro[] biblioteca = new Libro[500];
+int indice = 0;
+
+bool continua = true;
+while (continua)
+{
+    //sintassi di creazione di un oggetto da una classe
+    //nome classe //nome oggetto //parola chiave // nome classe
+    Libro oggettoLibro = new Libro();
+
+    Console.WriteLine("inserisci il titolo:");
+    oggettoLibro.Titolo = Console.ReadLine();
+
+    Console.WriteLine("inserisci l'autore:");
+    oggettoLibro.Autore = Console.ReadLine();
+
+    Console.WriteLine("inserisci il numero di pagine:");
+    oggettoLibro.Pagine = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("inserisci il prezzo:");
+    oggettoLibro.Prezzo = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Il prezzo in dollari sarà: " + oggettoLibro.PrezzoInDollari());
+
+    biblioteca[indice] = oggettoLibro;
+    indice++;
+
+    Console.WriteLine("Vuoi inserire un altro libro (S/N)?");
+    string risposta = Console.ReadLine();
+    if (risposta == "N") continua = false;
+}
